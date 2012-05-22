928c6798de54a03a952b5d51d30f7d9fda139a71
vikas srivastava <viky2130@gmail.com>
2012-05-22 22:26:23 +0530

    form for taking Url as input

---
+++ code/ExtensionAdmin.php
@@ -0,0 +1,10 @@
+<?php
+class ExtensionAdmin extends ModelAdmin {
+	static $menu_title = "Extensions";
+	static $url_segment = "extensions";
+
+	static $managed_models = array(
+		"ModuleUrlSubmission",
+		"Member",
+	);
+}	
