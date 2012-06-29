1019a659347e144aa9e88b24ccd97325efc55b2d
vikas srivastava <viky2130@gmail.com>
2012-06-29 16:49:04 +0530

    test for module pages

--- tests/ModuleTest.php
+++ tests/ModuleTest.php
@@ -1,6 +1,20 @@
 <?php
 
 class ModuleTest extends FunctionalTest{
-	static $fixture_file = 'extensionmanager/tests/ModuleTest.yml';
 	
-}  
+	function testIfModulePageExist() {
+		static $fixture_file = 'extensionmanager/tests/ExtensionManagerTest.yml';
+		
+		$testModule = $this->objFromFixture(
+			'Module',
+			'testmodule'
+			);
+		$response = $this->get('module/show/test-url');
+
+		$this->assertContains(
+			'Sorry that Module could not be found',
+			$response->getBody(),
+			'page not found'
+			);
+	}
+}
