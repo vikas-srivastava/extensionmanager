b7a910346ada5a03c7fc322f598e8248bb4c90f5
vikas srivastava <viky2130@gmail.com>
2012-06-29 16:50:43 +0530

    test for module pages

--- tests/JsonHandlerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-class JsonHandlerTest extends SapphireTest {
-	function testcloneJson() {
-		$repo = $this->getMock('VcsRepository');
-		
-		$driver = $repo->getDriver();
-		$this->assertType('VcsRepository', $driver); 
-	}
-}		
