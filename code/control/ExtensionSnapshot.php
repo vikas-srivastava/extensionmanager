ac1b8c3ba53f34d0888be3456b9e1d4c0d8654cb
vikas srivastava <viky2130@gmail.com>
2012-08-19 19:26:43 +0530

    Test for ExtensionSnapshot class

--- code/control/ExtensionSnapshot.php
+++ code/control/ExtensionSnapshot.php
@@ -18,7 +18,12 @@ class ExtensionSnapshot extends Controller {
 		
 		$folderToSave = 'assets/Uploads/Snapshots/'; 
 
-		$folderObject = Folder::get()->filter("Filename" , $folderToSave)->first(); 
+		$folderObject = Folder::get()->filter("Filename" , $folderToSave)->first();
+
+		if(!$folderObject) {
+			$folderObject = Folder::find_or_make('Uploads/Snapshots/');
+			$folderObject->write();
+		}
 
 		$fileExtension = preg_replace('/^.*\.([^.]+)$/D', '$1', $thumbnailUrl);
 
@@ -32,7 +37,13 @@ class ExtensionSnapshot extends Controller {
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
 		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
 		$data = curl_exec($ch);
+		$response = curl_getinfo($ch);
 		curl_close($ch);
+
+		if($response['http_code'] == '404') {
+			throw new InvalidArgumentException(
+				"Image not found on given Url Please check 'Snapshot' field in composer.json file");
+		}
 
 		$imageContent = $data;
 
@@ -40,17 +51,19 @@ class ExtensionSnapshot extends Controller {
 			$thumbnailFile = fopen(BASE_PATH.DIRECTORY_SEPARATOR.$folderToSave.$thumbnailName, 'w');
 			fwrite($thumbnailFile, $imageContent); 
 			fclose($thumbnailFile); 
+		} else {
+			throw new InvalidArgumentException("Could not create $folderToSave , Please create it mannually ");
 		}
 
 		if($thumbnailObject = Image::get()->filter("Name" , $thumbnailName)->first()) {
-        	return $thumbnailObject->ID ; 
-        } else {
-        	$thumbnailObject = new Image();
-            $thumbnailObject->ParentID = $folderObject->ID; 
-            $thumbnailObject->Name = $thumbnailName;
-            $thumbnailObject->OwnerID = (Member::currentUser() ? Member::currentUser()->ID : 0);
-            $thumbnailObject->write();
-            return $thumbnailObject->ID ;
-        }
-    }
+			return $thumbnailObject->ID ;
+		} else {
+			$thumbnailObject = new Image();
+			$thumbnailObject->ParentID = $folderObject->ID;
+			$thumbnailObject->Name = $thumbnailName;
+			$thumbnailObject->OwnerID = (Member::currentUser() ? Member::currentUser()->ID : 0);
+			$thumbnailObject->write();
+			return $thumbnailObject->ID ;
+		}
+	}
 }
---
+++ tests/control/ExtensionSnapshotTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* @package extensionmanager
+* @subpackage tests
+*/
+class ExtensionSnapshotTest extends SapphireTest {
+
+	static $fixture_file = 'extensionmanager/tests/ExtensionManagerTest.yml';
+
+	function testSaveSnapshot() {
+		
+		$obj = $this->objFromFixture('ExtensionData', 'testmodule');
+		$realUrl = 'http://openbees.org/images/Demo.jpg';
+
+		$imageId = ExtensionSnapshot::saveSnapshot($realUrl, $obj->Name);
+		$image = Image::get()->byID($imageId);
+
+		$this->assertFileExists(BASE_PATH.DIRECTORY_SEPARATOR.$image->Filename);
+	}
+
+	/**
+     * @expectedException InvalidArgumentException
+     */
+	function testSaveSnapshotException() {
+
+		$obj = $this->objFromFixture('ExtensionData', 'testmodule');
+		$fakeUrl = 'http://openbees.org/images/FakeImage.jpg';
+
+		ExtensionSnapshot::saveSnapshot($fakeUrl, $obj->Name);
+	}
+}	
